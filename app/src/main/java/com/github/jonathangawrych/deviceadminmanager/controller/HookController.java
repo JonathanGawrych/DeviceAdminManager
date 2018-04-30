2d549899d670fd724c425314f44c7ca8c9d074b1
Jonathan Gawrych <Jonathan.Gawrych@outlook.com>
2018-04-29 23:40:11 -0700

    Add Froyo device manager api loggers

--- app/src/main/java/com/github/jonathangawrych/deviceadminmanager/controller/HookController.java
+++ app/src/main/java/com/github/jonathangawrych/deviceadminmanager/controller/HookController.java
@@ -1,6 +1,17 @@
 package com.github.jonathangawrych.deviceadminmanager.controller;
 
+import android.annotation.TargetApi;
+import android.app.admin.DeviceAdminInfo;
+import android.app.admin.DeviceAdminReceiver;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Parcel;
 import android.util.Log;
+import android.util.Printer;
 
 import java.util.Arrays;
 
@@ -18,6 +29,88 @@ public class HookController implements IXposedHookZygoteInit {
 	public void initZygote(StartupParam startupParam) throws Throwable {
 		Log.i(TAG, TAG + " loaded");
 	
+		HookFroyo();
+	}
+	
+	@TargetApi(Build.VERSION_CODES.FROYO)
+	private void HookFroyo() {
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.FROYO)
+			return;
+		
+		Log.d(TAG, "Hooking Froyo Admin Methods");
+		
+		// remember constructor
+		attemptHookMethod(DeviceAdminInfo.class, "describeContents");
+		attemptHookMethod(DeviceAdminInfo.class, "dump", Printer.class, String.class);
+		attemptHookMethod(DeviceAdminInfo.class, "getActivityInfo");
+		attemptHookMethod(DeviceAdminInfo.class, "getComponent");
+		attemptHookMethod(DeviceAdminInfo.class, "getPackageName");
+		attemptHookMethod(DeviceAdminInfo.class, "getReceiverName");
+		attemptHookMethod(DeviceAdminInfo.class, "getTagForPolicy", int.class);
+		attemptHookMethod(DeviceAdminInfo.class, "isVisible");
+		attemptHookMethod(DeviceAdminInfo.class, "loadDescription", PackageManager.class);
+		attemptHookMethod(DeviceAdminInfo.class, "loadIcon", PackageManager.class);
+		attemptHookMethod(DeviceAdminInfo.class, "loadLabel", PackageManager.class);
+		attemptHookMethod(DeviceAdminInfo.class, "toString");
+		attemptHookMethod(DeviceAdminInfo.class, "usesPolicy", int.class);
+		attemptHookMethod(DeviceAdminInfo.class, "writeToParcel", Parcel.class, int.class);
+		
+		// DeviceAdminInfo Constants:
+		// USES_POLICY_FORCE_LOCK
+		// USES_POLICY_LIMIT_PASSWORD
+		// USES_POLICY_RESET_PASSWORD
+		// USES_POLICY_WATCH_LOGIN
+		// USES_POLICY_WIPE_DATA
+		// remember Fields
+		
+		attemptHookMethod(DevicePolicyManager.class, "getCurrentFailedPasswordAttempts");
+		attemptHookMethod(DevicePolicyManager.class, "getMaximumFailedPasswordsForWipe", ComponentName.class);
+		attemptHookMethod(DevicePolicyManager.class, "getPasswordMaximumLength", int.class);
+		attemptHookMethod(DevicePolicyManager.class, "getPasswordMinimumLength", ComponentName.class);
+		attemptHookMethod(DevicePolicyManager.class, "getPasswordQuality", ComponentName.class);
+		attemptHookMethod(DevicePolicyManager.class, "isActivePasswordSufficient");
+		attemptHookMethod(DevicePolicyManager.class, "isAdminActive", ComponentName.class);
+		attemptHookMethod(DevicePolicyManager.class, "removeActiveAdmin", ComponentName.class);
+		attemptHookMethod(DevicePolicyManager.class, "resetPassword", String.class, int.class);
+		attemptHookMethod(DevicePolicyManager.class, "setMaximumFailedPasswordsForWipe", ComponentName.class, int.class);
+		attemptHookMethod(DevicePolicyManager.class, "setMaximumTimeToLock", ComponentName.class, long.class);
+		attemptHookMethod(DevicePolicyManager.class, "setPasswordMinimumLength", ComponentName.class, int.class);
+		attemptHookMethod(DevicePolicyManager.class, "setPasswordQuality", ComponentName.class, int.class);
+		attemptHookMethod(DevicePolicyManager.class, "wipeData", int.class);
+		
+		// DevicePolicyManager Constants:
+		// ACTION_ADD_DEVICE_ADMIN
+		// ACTION_SET_NEW_PASSWORD
+		// EXTRA_ADD_EXPLANATION
+		// EXTRA_DEVICE_ADMIN
+		// PASSWORD_QUALITY_ALPHABETIC
+		// PASSWORD_QUALITY_ALPHANUMERIC
+		// PASSWORD_QUALITY_NUMERIC
+		// PASSWORD_QUALITY_SOMETHING
+		// PASSWORD_QUALITY_UNSPECIFIED
+		// RESET_PASSWORD_REQUIRE_ENTRY
+		
+		// TODO: hooks into anything but "onReceive" will be overridden
+		// Additionally "onReceive" could be overriden causing us to lose control of the receiver
+		attemptHookMethod(DeviceAdminReceiver.class, "getManager", Context.class);
+		attemptHookMethod(DeviceAdminReceiver.class, "getWho", Context.class);
+		attemptHookMethod(DeviceAdminReceiver.class, "onDisableRequested", Context.class, Intent.class);
+		attemptHookMethod(DeviceAdminReceiver.class, "onDisabled", Context.class, Intent.class);
+		attemptHookMethod(DeviceAdminReceiver.class, "onEnabled", Context.class, Intent.class);
+		attemptHookMethod(DeviceAdminReceiver.class, "onPasswordChanged", Context.class, Intent.class);
+		attemptHookMethod(DeviceAdminReceiver.class, "onPasswordFailed", Context.class, Intent.class);
+		attemptHookMethod(DeviceAdminReceiver.class, "onPasswordSucceeded", Context.class, Intent.class);
+		attemptHookMethod(DeviceAdminReceiver.class, "onReceive", Context.class, Intent.class);
+		
+		// DeviceAdminReceiver Constants:
+		// ACTION_DEVICE_ADMIN_DISABLED
+		// ACTION_DEVICE_ADMIN_DISABLE_REQUESTED
+		// ACTION_DEVICE_ADMIN_ENABLED
+		// ACTION_PASSWORD_CHANGED
+		// ACTION_PASSWORD_FAILED
+		// ACTION_PASSWORD_SUCCEEDED
+		// DEVICE_ADMIN_META_DATA
+		// EXTRA_DISABLE_WARNING
 	}
 	
 	private static XC_MethodHook passThough(final String name) {
